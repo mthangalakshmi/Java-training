e14b7e3aa4261de0b04b0c65c202e3ade1b8a02a
mthangalakshmi <72960570+mthangalakshmi@users.noreply.github.com>
2023-04-19 14:08:18 +0530

    April 11th task

---
+++ MyDBConnection.java
@@ -0,0 +1,26 @@
+package newJava;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class MyDBConnection {
+	static Connection con;
+
+	public static Connection getDbConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Password");
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getDbConnection());
+	}
+}
